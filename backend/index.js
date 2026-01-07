c0e2da291127b24ffdf0cb16a142dfd20a121c49
Dhvanit Raval <dhvanitraval.538@gmail.com>
2026-01-07 10:59:30 +0530

    URL fix

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5174", "http://localhost:5173", "https://dhvanit-raval.netlify.app/orders", "https://dhvanit-raval.netlify.app/holdings", "https://dhvanit-raval.netlify.app/positions", "https://dhvanit-raval.netlify.app/"],
+    origin: ["http://localhost:3000", "http://localhost:5174", "http://localhost:5173", "https://dhvanit-raval.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
